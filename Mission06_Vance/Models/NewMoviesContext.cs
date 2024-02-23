16472dec47221b329835201ff65fd828e721847d
prestonvance <prestonvance@PRESTONVANC1559>
2024-02-23 15:31:55 -0700

    added some comments and changed connection in appsettings.json

--- Mission06_Vance/Models/NewMoviesContext.cs
+++ Mission06_Vance/Models/NewMoviesContext.cs
@@ -2,9 +2,9 @@
 
 namespace Mission06_Vance.Models
 {
-    public class NewMoviesContext : DbContext
+    public class NewMoviesContext : DbContext //liaison from the app to the database
     {
-        public NewMoviesContext(DbContextOptions<NewMoviesContext> options) : base (options) 
+        public NewMoviesContext(DbContextOptions<NewMoviesContext> options) : base (options) // constructor
         { 
         }
 
